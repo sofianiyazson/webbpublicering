855e9577f2f1e52302896e3e78a948890d1efdef
Sofia Niyazson <rakhimaxon@gmail.com>
2022-08-18 22:39:54 +0200

    config vars

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send(process.env.DUMMY_TEXT);
 });
 
 app.listen(PORT, () => {
